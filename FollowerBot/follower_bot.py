31e7fddcfa911e28f64e7e2ba36949152493ffe1
Devin Wright <13djwright@gmail.com>
2016-09-21 19:40:42 -0400

    Add FollwerBot code

--- FollowerBot/follower_bot.py
+++ FollowerBot/follower_bot.py
@@ -1,43 +1,49 @@
-#!/usr/bin/python
-import praw, time, sys, sqlite3, re, smtplib
+#!/usr/bin/python3
+import praw, configparser, pprint, sqlite3, re, smtplib
 
-#------Bot Configuration------#
-BOTNAME     = 'FollowerBot'
-USERAGENT   = 'send comments and submission info via email from subscribed users. Author: /u/13djwright'
-USERS_TO_FOLLOW = []
-MESSAGE     = ""
-CHANGES     = 0
-server      = smtplib.SMTP("smtp.gmail.com",587)
+#-----Setting Up Configuration-----#
+ini_file = "config.ini"
+users_to_follow = []
+MESSAGE = ""
+CHANGES = 0
+
+config = configparser.ConfigParser(allow_no_value=True)
+config.read(ini_file)
+
+user_name = config['User']['name']
+password = config['User']['password']
+email = config['User']['email']
+posts_file = config['SQL']['posts_file']
+smtp_email = config['SMTP']['email']
+smtp_password = config['SMTP']['password']
+
+user_agent = "Follower script by " + user_name
+
+server = smtplib.SMTP("smtp.gmail.com",587)
+server.ehlo()
 server.starttls()
+server.login(smtp_email,smtp_password)
+r = praw.Reddit(user_agent=user_agent)
+
 #------SQL Database------#
 print('\nSetting up database...')
-sql = sqlite3.connect('/home/devin/code/side_projects/reddit/FollowerBot/posts.db')
+sql = sqlite3.connect(posts_file)
 cur = sql.cursor()
-cur.execute('create table if not exists oldposts(id TEXT)')
+cur.execute('CREATE TABLE IF NOT EXISTS oldposts(id TEXT)')
 
 sql.commit()
 print('DONE')
 
-#------Reddit Login------#
-print('Logging in to Reddit...')
-r = praw.Reddit(user_agent=USERAGENT)
-r.login("13djwright","tiger0082")
-print('DONE')
-for row in cur.execute('select name from users where name is not "13djwright"'):
-    USERS_TO_FOLLOW.append(str(row[0]))
-
-EMAIL    = str(cur.execute('select email from users where name is "13djwright"').fetchone()[0])
-PASSWORD = str(cur.execute('select password from users where name is "13djwright"').fetchone()[0])
-
-server.login(EMAIL,PASSWORD)
+for user in config['To Follow']:
+    users_to_follow.append(str(user))
 
 #---------HELPER FUNCTIONS---------#
 
 #------This function should be able to handle submissions and comments, checking whether it has been seen, if not adding it to the database table------#
 def is_new_post(post_id, post_author):
-    cur.execute('select * from oldposts where id=?',(post_id,))
-    if not cur.fetchone() and post_author.lower() != BOTNAME.lower():
-        cur.execute('insert into oldposts values(?)', (post_id,))
+    cur.execute('SELECT * FROM oldposts WHERE id=?',(post_id,))
+    if not cur.fetchone():
+        cur.execute('INSERT INTO oldposts VALUES(?)', (post_id,))
         sql.commit()
         return True
     else:
@@ -67,16 +73,18 @@ def process_submissions(posts, is_comment, user_name):
             CHANGES += 1
 
 def main():
-    for u in USERS_TO_FOLLOW:
-       print("Getting submissions from " + u)
+    for u in users_to_follow:
+       print("Getting submissions from " + u + "\n")
        user = r.get_redditor(u)
        sub_posts = user.get_submitted(sort='new',limit=10)
        com_posts = user.get_comments(sort='new',limit=10)
        process_submissions(sub_posts, False, u)
        process_submissions(com_posts, True, u)
     if CHANGES > 0:
-        print("New changes found, emailing now")
-        emailMessage = "\r\n".join(["From: follower_bot@reddit.com", "To: 13djwright@gmail.com", "Subject: Updated Subs and Comments from stalker_bot", "", MESSAGE])
-        server.sendmail('follower_bot@reddit.com', '13djwright@gmail.com', emailMessage) 
+        from_e = "From: follower_bot"
+        to_e = "To: " + email
+        print("New changes found, emailing now\n")
+        emailMessage = "\r\n".join([from_e, to_e, "Subject: Updated Submissions and Comments from FollowerBot", "", MESSAGE])
+        server.sendmail('follower_bot@reddit.com', email, emailMessage) 
        
 main()  
--- README.md
+++ README.md
@@ -6,4 +6,23 @@ Reddit bots written in Python to do various functions. (Currently only 1 finishe
 <h5>Follower Bot</h5>
 
 A bot used to follow certain users and have their posts and comments emailed to you. Best run as a cron job every few minutes. 
+Relies on a config.ini file that looks like the following:
+
+[User]
+name=username
+password=password
+email=email_to_recieve@email.com
+
+[SMTP]
+email=email_to_send@email.com
+password=password
+
+[To Follow]
+user1
+user2
+user3
+
+[SQL]
+posts_file=/path/to/posts/database/posts.db
+
 
